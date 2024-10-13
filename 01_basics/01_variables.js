b75a2476e78cbaf1983862bdf21d211b5abcf84a
Musab Khalid Khan <godxxillaa@gmail.com>
2024-10-13 18:18:44 +0000

    learned variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "x@gmail.com"
+var accountPassword = "12345"
+accountCity = "Dubai"
+let x;
+
+// accountId = 2
+
+accountEmail = "y@gmail.com"
+accountPassword = "212121"
+accountCity = "Karachi"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([accountEmail, accountPassword , accountCity, x])
+
+

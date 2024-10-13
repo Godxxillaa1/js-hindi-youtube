57ac1dc467eaa3fb21c21e7fc5121e916f373c5e
Musab Khalid Khan <godxxillaa@gmail.com>
2024-10-13 21:15:46 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,46 @@
+// Extra Functions:
+function gapAdder (sign, times) {
+
+    let gap = sign ;
+    let multiGap = gap.repeat(times) ;
+    console.log("");
+    console.log(multiGap);
+    console.log("");
+    
+};
+
+
+
+
+let score = false;
+// console.log(typeof(score));
+let valueInNumber = Number(score);
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" can be converted in 33
+// "33abc" can not convert in a number log will reply NaN
+// True and False can be converted in a number log will reply (1 for True) & (0 for False)
+// undefined cannot be converted in a number log will reply NaN
+// Null can be converted int a number it will reply 0
+
+// gapAdder("-", 89);
+
+let isLoggedIn = " ";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// In boolean conversion 
+// 1 or >1 possitive or nagative will be true and 0 will be true is well
+// empty string is converting in false and sting with sonthing in it is returning a True
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
+

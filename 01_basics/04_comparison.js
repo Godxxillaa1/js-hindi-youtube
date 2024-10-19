01da4fa879fce7bd0121ca668df2ce750391612f
Musab Khalid Khan <godxxillaa@gmail.com>
2024-10-19 13:01:02 +0000

    04_comparison is completed

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+
+// console.log("2" > 2);
+// console.log("02" > 2);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// Strict Check
+console.log(2 === 2);
